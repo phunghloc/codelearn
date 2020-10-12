61fc6f3c8e5994fab65bf149350787430bac1bc7
PHLoc <phunghloc@gmail.com>
2020-10-12 13:42:41 +0700

    Updates

---
+++ precache-manifest.6e027f68aa08cd671d1fcf6ae058a5f9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e3e1906fded057c2602b3ab3b04b3f",
+    "url": "/codelearn/index.html"
+  },
+  {
+    "revision": "971f7ec330ae8bafd26f",
+    "url": "/codelearn/static/css/2.bd2f563d.chunk.css"
+  },
+  {
+    "revision": "1e305dee79f7aa005221",
+    "url": "/codelearn/static/css/main.84365166.chunk.css"
+  },
+  {
+    "revision": "971f7ec330ae8bafd26f",
+    "url": "/codelearn/static/js/2.c8288c7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/codelearn/static/js/2.c8288c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e305dee79f7aa005221",
+    "url": "/codelearn/static/js/main.16ddb309.chunk.js"
+  },
+  {
+    "revision": "3db2b9c21281bad56a79",
+    "url": "/codelearn/static/js/runtime-main.12eb3cbd.js"
+  },
+  {
+    "revision": "9a00a181162025f4f3fd3ffaebcd73fb",
+    "url": "/codelearn/static/media/chia-khoa-lap-trinh.9a00a181.png"
+  },
+  {
+    "revision": "a296d0aa23f4624d2b6a75811cc34aa5",
+    "url": "/codelearn/static/media/codelearn-logo.a296d0aa.svg"
+  },
+  {
+    "revision": "0afde7866a28d51d4a0bfd225d984909",
+    "url": "/codelearn/static/media/head-left-bg.0afde786.png"
+  },
+  {
+    "revision": "2aad0756293f53dd7c3235a36a27855b",
+    "url": "/codelearn/static/media/logo-fpt-footer.2aad0756.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/codelearn/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/codelearn/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/codelearn/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/codelearn/static/media/slick.f97e3bbf.svg"
+  }
+]);
